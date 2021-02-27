0b3451bd52ae67f64f8f6476159f63cf8c7f9ce2
Kidataek <tuankiet.hcmc@gmail.com>
2021-02-28 00:25:12 +0700

    feat: add gmail api

--- Mail.ts
+++ Mail.ts
@@ -1,99 +1,156 @@
-import * as Mailer from "nodemailer";
-import * as Fs from "fs";
-import { hostname } from "os";
-import { info, debug, error } from "./Log";
+import * as Mailer from 'nodemailer';
+import * as Fs from 'fs';
+import smtpTransport from 'nodemailer-smtp-transport';
+import { hostname } from 'os';
+import { info, debug, error } from './Log';
 
 export interface ISmtpConfig {
-    smtp: {
-        host?: string;
-        port?: number;
-        user?: string;
-        password?: string;
-        secure?: boolean;
-        from?: string;
-        disabled: boolean;
-    },
-    mailTo: string;
-    replyTo: string;
-    batchPeriodM?: number;
-    batchMaxMessages?: number;
+  smtp: {
+    host?: string;
+    port?: number;
+    user?: string;
+    password?: string;
+    secure?: boolean;
+    from?: string;
+    disabled: boolean;
+  };
+  gmail: {
+    type?: string;
+    user?: string;
+    clientId?: string;
+    clientSecret?: string;
+    refreshToken?: string;
+    disabled: boolean;
+  };
+  mailTo: string;
+  replyTo: string;
+  batchPeriodM?: number;
+  batchMaxMessages?: number;
 }
 
 export interface IMessage {
-    subject: string;
-    body: string;
-    priority?: "high" | "low";
-    attachements?: any[];
-    on?: Date
+  subject: string;
+  body: string;
+  priority?: 'high' | 'low';
+  attachements?: any[];
+  on?: Date;
 }
 
 export class Mail {
-    private _template = "<p><!-- body --></p><p><!-- timeStamp --></p>";
+  protected _template = '<p><!-- body --></p><p><!-- timeStamp --></p>';
 
-    constructor(private _config: ISmtpConfig) {
-        if (!this._config.smtp)
-            this._config.smtp = { disabled: true };
+  constructor(protected _config: ISmtpConfig) {
+    if (!this._config.smtp) this._config.smtp = { disabled: true };
 
-        if (this._config.smtp.disabled === true)
-            return; // don't analyze config if disabled
+    if (this._config.smtp.disabled === true) return; // don't analyze config if disabled
 
-        if (!this._config.smtp)
-            throw new Error(`[smtp] not set`);
-        if (!this._config.smtp.host)
-            throw new Error(`[smtp.host] not set`);
-        if (!this._config.smtp)
-            throw new Error(`[smtp.port] not set`);
+    if (!this._config.smtp) throw new Error(`[smtp] not set`);
+    if (!this._config.smtp.host) throw new Error(`[smtp.host] not set`);
+    if (!this._config.smtp) throw new Error(`[smtp.port] not set`);
 
-        try {
-            this._template = Fs.readFileSync("Template.html", "utf8");
-        }
-        catch {
-            info(`Template.html not found`);
-        }
+    try {
+      this._template = Fs.readFileSync('Template.html', 'utf8');
+    } catch {
+      info(`Template.html not found`);
+    }
+  }
+
+  configChanged() {
+    if (!this._config.mailTo) throw new Error(`[mailTo] not set`);
+  }
+
+  async send(message: IMessage) {
+    if (this._config.smtp.disabled === true) {
+      debug('mail sending is disbled in config');
+      return;
     }
 
-    configChanged() {
-        if (!this._config.mailTo)
-            throw new Error(`[mailTo] not set`);
+    const temp = {
+      host: this._config.smtp.host,
+      port: this._config.smtp.port,
+      tls: { rejectUnauthorized: false },
+      secure: this._config.smtp.secure === true,
+      auth: null
+    };
+
+    if (this._config.smtp.user)
+      temp.auth = {
+        user: this._config.smtp.user,
+        pass: this._config.smtp.password
+      };
+
+    const transport = Mailer.createTransport(temp),
+      headers = {};
+
+    if (message.priority) headers['importance'] = message.priority;
+
+    await transport.sendMail({
+      to: this._config.mailTo,
+      from: this._config.smtp.from || this._config.smtp.user, // use from, if not set -> user
+      replyTo: this._config.replyTo,
+      subject: `pm2-health: ${hostname()}, ${message.subject}`,
+      html: this._template
+        .replace(/<!--\s*body\s*-->/, message.body)
+        .replace(/<!--\s*timeStamp\s*-->/, new Date().toISOString()),
+      attachments: message.attachements,
+      headers
+    });
+  }
+}
+
+export class GMail extends Mail {
+  constructor(_config: ISmtpConfig) {
+    super(_config);
+
+    if (!this._config.gmail) this._config.gmail = { disabled: true };
+
+    if (this._config.gmail.disabled === true) return; // don't analyze config if disabled
+
+    if (!this._config.gmail) throw new Error(`[gmail] not set`);
+    if (!this._config.gmail.type) throw new Error(`[smtp.type] not set`);
+    if (!this._config.gmail.clientId)
+      throw new Error(`[smtp.clientId] not set`);
+    if (!this._config.gmail.clientSecret)
+      throw new Error(`[smtp.clientSecret] not set`);
+    if (!this._config.gmail.user) throw new Error(`[smtp.user] not set`);
+    if (!this._config.gmail.refreshToken)
+      throw new Error(`[smtp.refreshToken] not set`);
+  }
+
+  async send(message: IMessage) {
+    if (this._config.gmail.disabled === true) {
+      debug('mail sending is disbled in config');
+      return;
     }
 
-    async send(message: IMessage) {
-        if (this._config.smtp.disabled === true) {
-            debug("mail sending is disbled in config");
-            return;
-        }
+    const auth = {
+      type: 'oauth2',
+      user: this._config.gmail.user,
+      clientId: this._config.gmail.clientId,
+      clientSecret: this._config.gmail.clientSecret,
+      refreshToken: this._config.gmail.refreshToken
+    };
 
-        const temp = {
-            host: this._config.smtp.host,
-            port: this._config.smtp.port,
-            tls: { rejectUnauthorized: false },
-            secure: this._config.smtp.secure === true,
-            auth: null
-        };
+    const transport = Mailer.createTransport(
+      smtpTransport({
+        service: 'gmail',
+        auth: auth
+      })
+    );
+    const headers = {};
 
-        if (this._config.smtp.user)
-            temp.auth = {
-                user: this._config.smtp.user,
-                pass: this._config.smtp.password
-            };
+    if (message.priority) headers['importance'] = message.priority;
 
-        const
-            transport = Mailer.createTransport(temp),
-            headers = {};
-
-        if (message.priority)
-            headers["importance"] = message.priority;
-
-        await transport.sendMail({
-            to: this._config.mailTo,
-            from: this._config.smtp.from || this._config.smtp.user, // use from, if not set -> user
-            replyTo: this._config.replyTo,
-            subject: `pm2-health: ${hostname()}, ${message.subject}`,
-            html: this._template
-                .replace(/<!--\s*body\s*-->/, message.body)
-                .replace(/<!--\s*timeStamp\s*-->/, new Date().toISOString()),
-            attachments: message.attachements,
-            headers
-        });
-    }
+    await transport.sendMail({
+      to: this._config.mailTo,
+      from: this._config.smtp.from || this._config.smtp.user, // use from, if not set -> user
+      replyTo: this._config.replyTo,
+      subject: `pm2-health: ${hostname()}, ${message.subject}`,
+      html: this._template
+        .replace(/<!--\s*body\s*-->/, message.body)
+        .replace(/<!--\s*timeStamp\s*-->/, new Date().toISOString()),
+      attachments: message.attachements,
+      headers
+    });
+  }
 }
--- Notify.ts
+++ Notify.ts
@@ -1,97 +1,100 @@
-import { Mail, IMessage, ISmtpConfig } from "./Mail";
-import { debug, error } from "./Log";
+import { GMail as Mail, IMessage, ISmtpConfig } from './Mail';
+import { debug, error } from './Log';
 
 // todo: add messages persistance in case of crash
 
 export class Notify {
-    private readonly _mail: Mail;
-    private _holdTill: Date = null;
-    private _messages: IMessage[] = [];
-    private _t: NodeJS.Timer;
+  private readonly _mail: Mail;
+  private _holdTill: Date = null;
+  private _messages: IMessage[] = [];
+  private _t: NodeJS.Timer;
 
-    constructor(private readonly _config: ISmtpConfig) {
-        this._mail = new Mail(this._config);
+  constructor(private readonly _config: ISmtpConfig) {
+    this._mail = new Mail(this._config);
+  }
+
+  private get isEnabled() {
+    return this._config.batchPeriodM > 0;
+  }
+
+  configChanged() {
+    this._mail.configChanged();
+
+    clearInterval(this._t);
+
+    if (this.isEnabled) {
+      debug(
+        `message batching is enabled, period = ${this._config.batchPeriodM} minutes`
+      );
+
+      this._t = setInterval(async () => {
+        if (this._messages.length > 0) await this.sendBatch();
+      }, this._config.batchPeriodM * 60 * 1000);
     }
+  }
 
-    private get isEnabled() {
-        return this._config.batchPeriodM > 0;
+  hold(till: Date) {
+    this._holdTill = till;
+  }
+
+  private async sendBatch() {
+    const snapshot = this._messages.slice(); // make snapshot of messages
+    this._messages = [];
+
+    let body = '',
+      i = 1;
+
+    for (const message of snapshot)
+      body += `-------- ${i++} | ${
+        message.subject
+      } | ${message.on.toISOString()} --------<br/>${message.body}`;
+
+    const temp: IMessage = {
+      subject: `${snapshot[0].subject} (${snapshot.length} items)`,
+      body,
+      attachements: snapshot
+        .filter((e) => e.attachements != null)
+        .map((e) => e.attachements)
+        .reduce((p, c) => p.concat(c), [])
+    };
+
+    try {
+      await this._mail.send(temp);
+
+      debug(`batch of ${snapshot.length} messages sent`);
+    } catch (ex) {
+      // restore messages if sent fail
+      this._messages.unshift(...snapshot);
+
+      error(`can't send batch mail -> ${ex.message || ex}`);
     }
+  }
 
-    configChanged() {
-        this._mail.configChanged();
+  async send(message: IMessage) {
+    const t = new Date();
 
-        clearInterval(this._t);
+    message.on = t;
 
-        if (this.isEnabled) {
-            debug(`message batching is enabled, period = ${this._config.batchPeriodM} minutes`);
+    if (this._holdTill != null && t < this._holdTill) return; // skip
 
-            this._t = setInterval(async () => {
-                if (this._messages.length > 0)
-                    await this.sendBatch();
+    if (this.isEnabled && message.priority !== 'high') {
+      debug(`message (batch) -> ${message.subject}`);
 
-            }, this._config.batchPeriodM * 60 * 1000);
-        }
+      this._messages.push(message);
+
+      if (
+        this._config.batchMaxMessages > 0 &&
+        this._messages.length >= this._config.batchMaxMessages
+      )
+        await this.sendBatch();
+    } else {
+      debug(`message -> ${message.subject}`);
+
+      try {
+        await this._mail.send(message);
+      } catch (ex) {
+        error(`can't send mail -> ${ex.message || ex}`);
+      }
     }
-
-    hold(till: Date) {
-        this._holdTill = till;
-    }
-
-    private async sendBatch() {
-        const snapshot = this._messages.slice();  // make snapshot of messages
-        this._messages = [];
-
-        let
-            body = "",
-            i = 1;
-
-        for (const message of snapshot)
-            body += `-------- ${i++} | ${message.subject} | ${message.on.toISOString()} --------<br/>${message.body}`;
-
-        const temp: IMessage = {
-            subject: `${snapshot[0].subject} (${snapshot.length} items)`,
-            body,
-            attachements: snapshot.filter(e => e.attachements != null).map(e => e.attachements).reduce((p, c) => p.concat(c), [])
-        }
-
-        try {
-            await this._mail.send(temp);
-
-            debug(`batch of ${snapshot.length} messages sent`);
-        }
-        catch (ex) {
-            // restore messages if sent fail
-            this._messages.unshift(...snapshot);
-
-            error(`can't send batch mail -> ${ex.message || ex}`);
-        }
-    }
-
-    async send(message: IMessage) {
-        const t = new Date();
-
-        message.on = t;
-
-        if (this._holdTill != null && t < this._holdTill)
-            return; // skip
-
-        if (this.isEnabled && message.priority !== "high") {
-            debug(`message (batch) -> ${message.subject}`);
-
-            this._messages.push(message);
-
-            if (this._config.batchMaxMessages > 0 && this._messages.length >= this._config.batchMaxMessages)
-                await this.sendBatch();
-        }
-        else {
-            debug(`message -> ${message.subject}`);
-
-            try {
-                await this._mail.send(message);
-            }
-            catch (ex) {
-                error(`can't send mail -> ${ex.message || ex}`);
-            }
-        }
-    }
+  }
 }
